e38e7bb5dcb3bfe2766cfc8e89609fd90881a260
Derek Peacock <derek.peacock@yahoo.com>
2022-06-08 13:53:25 +0100

    Added a main method

---
+++ Program.java
@@ -0,0 +1,12 @@
+public class Program 
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+        System.out.println("CO452 Programming Concepts 2022/23");
+        System.out.println("==================================");
+        System.out.println();
+        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println();
+    }
+}
