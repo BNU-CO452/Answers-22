e5f9354345884df29026badba4d11dd584f09156
Derek Peacock <derek.peacock@yahoo.com>
2022-06-08 13:56:57 +0100

    Added console clear

--- Program.java
+++ Program.java
@@ -1,8 +1,9 @@
 public class Program 
 {
+    public static final String CONSOLE_CLEAR = "\033[H\033[2J";
     public static void main(String[] args)
     {
-        System.out.println();
+        System.out.println(CONSOLE_CLEAR);
         System.out.println("CO452 Programming Concepts 2022/23");
         System.out.println("==================================");
         System.out.println();
