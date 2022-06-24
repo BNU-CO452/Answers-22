dfd95583bf98bedfb185dc7213fec57799c0169c
Derek Peacock <derek.peacock@yahoo.com>
2022-06-24 19:50:13 +0100

    input age works

--- Program.java
+++ Program.java
@@ -1,6 +1,9 @@
 public class Program 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
+
+    //private int age;
+
     public static void main(String[] args)
     {
         System.out.println(CONSOLE_CLEAR);
@@ -9,6 +12,9 @@ public class Program
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
+
+        int age = InputReader.getInt("Please enter your age > ");
+        System.out.println("You were born in " + (2022 - age));
 
         Week01 exercise01 = new Week01();
         exercise01.run();
